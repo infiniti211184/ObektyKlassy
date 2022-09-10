70e86a42966b90a16165e8d6b62f496578fbb06f
Julia Khramova <infiniti211184@mail.ru>
2022-09-11 00:10:56 +0300

    Сделано ДЗ Объекты и классы

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.awt.print.Book;
+
+public class Main {
+    int Book;
+    static Book[] books = new Book[3];
+    int next = 0;
+
+    public static void main(String[] args) {
+        var library = new Library ( librarySize: 5);
+        library.add(new Book ( name: "name", author: null, publicationYear: 123));
+        library.add(new Book ( name: "name", author: null, publicationYear: 123));
+        library.add(new Book ( name: "name", author: null, publicationYear: 123));
+        library.add(new Book ( name: "name", author: null, publicationYear: 123));
+        library.add(new Book ( name: "name", author: null, publicationYear: 123));
+        library.add(new Book ( name: "name", author: null, publicationYear: 123));
+
+        addBook(new Book( name: "name1", author: null, publicationYear: 123));
+        addBook(new Book( name: "name1", author: null, publicationYear: 123));
+        addBook(new Book( name: "name1", author: null, publicationYear: 123));
+    }
+    public static void addBook(java.awt.print.Book) {
+        books[next] = book;
+        next = next + 1;
+    }
+}
